98f5f83a2cb9af7f5826eef2b7e4f7377358f452
Florian Thiel <flth@flth.de>
2024-11-29 17:51:19 +0100

    initial

---
+++ strings.lua
@@ -0,0 +1,15 @@
+function data()
+	info_name = "Flugzeug Asset Set 2"
+	info_desc = (
+				"Flugzeugset zum gestallten von Flugplatzscenen.")
+	return {
+		de = {
+			["mod_name"] = "Flugzeug Asset Set 2",
+			["mod_desc"] = "Flugzeugset zum Gestalten von Flugplatzszenen.",
+		},
+		en = {
+			["mod_name"] = "Aircraft Asset Set 2",
+			["mod_desc"] = "Aircraft collection to set up airfield scenes.",
+		},
+	}
+end
